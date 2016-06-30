40dbba794253630f9d41ffc6d61ad8c33af0dd68
Shawn Wildermuth <shawn@wildermuth.com>
2016-06-29 21:31:49 -0400

    Update to support reset passwrod

---
+++ src/CoreCodeCamp/Migrations/20160630013107_AddedSponsors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreCodeCamp.Migrations
+{
+    public partial class AddedSponsors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sponsors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Paid = table.Column<bool>(nullable: false),
+                    SponsorLevel = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sponsors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sponsors");
+        }
+    }
+}

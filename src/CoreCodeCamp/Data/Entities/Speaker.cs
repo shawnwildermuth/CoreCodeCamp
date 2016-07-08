fd2fea509503d28d681f15e3d0c7866489d45863
Shawn Wildermuth <shawn@wildermuth.com>
2016-07-08 07:56:15 -0400

    Conversion Complete for past years

--- src/CoreCodeCamp/Data/Entities/Speaker.cs
+++ src/CoreCodeCamp/Data/Entities/Speaker.cs
@@ -12,9 +12,10 @@ namespace CoreCodeCamp.Data.Entities
     public string Name { get; set; }
     public string UserName { get; set; }
     public string Website { get; set; }
-    public string TwitterHandle { get; set; }
+    public string Twitter { get; set; }
     public string Blog { get; set; }
     public string Bio { get; set; }
+    public string ImageUrl { get; set; }
     public string Title { get; set; }
     public string CompanyName { get; set; }
     public string CompanyUrl { get; set; }

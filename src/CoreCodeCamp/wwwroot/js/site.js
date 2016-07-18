47c4ff4c61565d8c3a5c03d672767e5783400a74
Shawn Wildermuth <shawn@wildermuth.com>
2016-07-17 20:03:54 -0400

    Tweak UI of accounts and fix errors with Favorites

--- src/CoreCodeCamp/wwwroot/js/site.js
+++ src/CoreCodeCamp/wwwroot/js/site.js
@@ -2,20 +2,40 @@
 $(document).ready(function () {
 
   $(".voteStar").on("click", function () {
-    var me = $(this);
-    var id = me.attr("data-id");
-    if (me.hasClass("voted")) {
-      me.removeClass("voted");
-    } else {
-      me.addClass("voted");
-    }
+    var $this = $(this);
     $.ajax({
-      url: "/api/talks/" + id + "/toggleStar",
+      url: "/api/talks/" + $this.attr("data-id") + "/toggleStar",
       type: 'PUT',
-      error: function (result) {
-        console.log("Failed to PUT to toggling the star of a talk")
+      success: function () {
+        var id = me.attr("data-id");
+        if ($this.hasClass("voted")) {
+          $this.removeClass("voted");
+        } else {
+          $this.addClass("voted");
+        }
+      },
+      error: function (error) {
+        if (error.status == 401) {
+          showAlert("Must be logged in to set favorite sessions.");
+        } else {
+          showAlert("Failed to set favorite. Unknown reason.");
+        }
       }
     });
   });
+
+  var $topBar = $("#topBar");
+  function showAlert(msg, alertType) {
+    if (!alertType) alertType = "danger";
+    var template = "<div class='alert alert-##ALERTTYPE## alert-dismissible'>" +
+          "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'><i class='fa fa-close'></i></span></button>" +
+          "<p>##MSG##</p>" +
+        "</div>";
+    var $alert = (template.replace("##MSG##", msg).replace("##ALERTTYPE##", alertType));
+    $topBar.after($alert);
+    $alert.alert();
+  }
+
+
 
 });

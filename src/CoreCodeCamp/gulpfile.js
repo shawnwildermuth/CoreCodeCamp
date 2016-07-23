ae6e21c009a5a8e9e2dd577b8d01aa7226a9542f
Shawn Wildermuth <shawn@wildermuth.com>
2016-07-23 02:05:10 -0400

    Fixing Scripts

--- src/CoreCodeCamp/gulpfile.js
+++ src/CoreCodeCamp/gulpfile.js
@@ -1,15 +1,8 @@
 /// <binding Clean='default' />
-/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require("gulp-rename");
-var bower = require('gulp-bower-files');
-var webpack = require('webpack-stream');
 
 gulp.task("npmTasks", function () {
   var libs = {
@@ -25,10 +18,12 @@ gulp.task("npmTasks", function () {
     gulp.src("node_modules/" + libs[name])
       .pipe(gulp.dest("wwwroot/lib/" + name));
   }
+
+  return;
 });
 
 gulp.task("min", function () {
-  gulp.src([ "wwwroot/js/site.js"])
+  return gulp.src([ "wwwroot/js/site.js"])
     .pipe(uglify())
     .pipe(rename("site.min.js"))
     .pipe(gulp.dest("wwwroot/lib/site/"));

cf01d8ecfe6ac24078ac61d4dee0a8efb6edccc0
Shawn Wildermuth <shawn@wildermuth.com>
2016-07-28 04:47:33 -0400

    Update for Final Delivery

--- src/CoreCodeCamp/Startup.cs
+++ src/CoreCodeCamp/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -88,7 +89,13 @@ namespace CoreCodeCamp
 
       svcs.AddScoped<IEmailTemplateEngine, EmailTemplateEngine>();
 
-      svcs.AddMvc();
+      svcs.AddMvc(opt =>
+      {
+        if (_env.IsProduction())
+        {
+          opt.Filters.Add(new RequireHttpsAttribute());
+        }
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

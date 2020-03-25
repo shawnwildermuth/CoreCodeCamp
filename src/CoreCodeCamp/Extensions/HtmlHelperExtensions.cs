436ed8b683e0e6b333d672b2bb70bfa337927d2d
Shawn Wildermuth <shawn@wildermuth.com>
2020-03-25 04:31:29 -0400

    Updating to fix sponsoring issue(s)

--- src/CoreCodeCamp/Extensions/HtmlHelperExtensions.cs
+++ src/CoreCodeCamp/Extensions/HtmlHelperExtensions.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace CoreCodeCamp.Extensions
 {
   public static class HtmlHelperExtensions
   {
-    public static HtmlString GetMoniker<T>(this IHtmlHelper<T> html) 
+    public static HtmlString GetMoniker<T>(this IHtmlHelper<T> html)
     {
       return new HtmlString(html.ViewContext.RouteData.Values["moniker"] as string);
+    }
+
+    public static bool HasView<dynamic>(this IHtmlHelper<dynamic> html, string viewName, ICompositeViewEngine engine)
+    {
+      var result = engine.FindView(html.ViewContext, viewName, false);
+      return result.Success;
     }
 
     public static HtmlString GetActiveMenuItem<T>(this IHtmlHelper<T> html, string controller, string action)
     {
       if (html.ViewContext.RouteData.Values.ContainsKey("controller") &&
-        html.ViewContext.RouteData.Values.ContainsKey("action") && 
-        html.ViewContext.RouteData.Values["controller"].Equals(controller) && 
+        html.ViewContext.RouteData.Values.ContainsKey("action") &&
+        html.ViewContext.RouteData.Values["controller"].Equals(controller) &&
         html.ViewContext.RouteData.Values["action"].Equals(action) &&
         html.ViewContext.RouteData.Values["area"] == null)
       {
@@ -32,7 +40,7 @@ namespace CoreCodeCamp.Extensions
 
     public static HtmlString MakeUrlAbsolute<T>(this IHtmlHelper<T> html, string url)
     {
-      if (url.ToLower().StartsWith("http://") || 
+      if (url.ToLower().StartsWith("http://") ||
         url.ToLower().StartsWith("https://"))
       {
         return new HtmlString(url);
